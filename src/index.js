2846dfe59997362b30ee088e319aca0ec97ed5ca
deadbeathd07 <deadbeat.hd.07@gmail.com>
2022-12-07 13:47:30 +0300

    feat: added a button mixin with styles for static and active states, added variables and the  main font, for both was created an individual file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "normalize.css";
 import "@styles";
+
+let buttons = document.querySelectorAll(".button");
+
+for (let button of buttons) {
+    button.addEventListener("mouseover", function () {
+        button.classList.toggle("button_active")
+    });
+    button.addEventListener("mouseout", function () {
+        button.classList.toggle("button_active");
+    });
+}
