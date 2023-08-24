a10cbaba97a6b0e96cbbe1beb12e080fc4ab0ddf
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-24 19:02:33 +0300

    выдалены непатрэбныя ўласцiвасцi аб'екта

--- src/vuetify/index.js
+++ src/vuetify/index.js
@@ -2,17 +2,10 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import LogoIcon from '../components/icons/LogoIcon.vue'
 
 const vuetify = createVuetify({
   components,
-  directives,
-  icons: {
-    iconfont: 'mdi',
-    values: {
-      logo: { component: LogoIcon }
-    }
-  }
+  directives
 })
 
 export default vuetify
