a4ffe50d6cb5266f5d73d9637ba4ffb657c9be46
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-24 19:15:17 +0300

    так як абразок тэлеграма зарэгiстраваны глабальна, лакальны яго iмпарт быў выдалены

--- src/components/RatioSocialNetworksBar.vue
+++ src/components/RatioSocialNetworksBar.vue
@@ -1,13 +1,12 @@
 <template>
   <v-btn icon v-for="(elem, key) in props.networks" :key="key" :href="elem">
     <v-icon color="#DD0426" v-if="key == 'email'">mdi-{{ key }}-outline</v-icon>
-    <TelegramIcon color="#DD0426" v-else-if="key == 'telegram'" />
+    <telegram-icon color="#DD0426" v-else-if="key == 'telegram'" />
     <v-icon color="#DD0426" v-else>mdi-{{ key }}</v-icon>
   </v-btn>
 </template>
 
 <script setup>
-import TelegramIcon from './icons/TelegramIcon.vue'
 const props = defineProps({
   networks: Object
 })
