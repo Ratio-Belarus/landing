d5e0f933053ab060b13493ca3d7a684fc4f6d4e2
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-24 18:56:22 +0300

    у сувязi з тым, што абразок лагатыпа быў зарэгiстраваны глабальна, яго лакальны iмпарт быў выдалены

---
+++ src/components/RatioHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar color="white" elevation="0" scroll-behavior="hide" class="mx-">
+    <v-app-bar-title>
+      <div class="d-flex align-center font-weight-bold">
+        <logo-icon :width="60" />
+        <p class="text-28 text-product-red">Рацыя</p>
+      </div>
+    </v-app-bar-title>
+    <v-spacer />
+    <RatioSocialNetworksBar :networks="props.networks" />
+    <v-btn color="#DD0426" variant="flat" class="text-white text-capitalize font-weight-medium">{{
+      $t('btn.join')
+    }}</v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+import RatioSocialNetworksBar from '../components/RatioSocialNetworksBar.vue'
+
+const props = defineProps({
+  networks: Object,
+})
+</script>
+
+<style scoped></style>
