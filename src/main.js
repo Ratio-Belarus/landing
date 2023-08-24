0055c54b78fa7ecbe075736602a647c41c8bb6ed
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-24 19:01:40 +0300

    кастамныя абразкi былi глабальна зарэгiстраваны

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import vuetify from './vuetify'
 import { createI18n } from 'vue-i18n'
 import messages from './locales/messages'
 import './assets/styles/index.css'
+import LogoIcon from './components/icons/LogoIcon.vue'
+import RulesIcon from './components/icons/RulesIcon.vue'
+import PracticeIcon from './components/icons/PracticeIcon.vue'
+import PrizeIcon from './components/icons/PrizeIcon.vue'
+import CompetitionIcon from './components/icons/CompetitionIcon.vue'
 
 const app = createApp(App)
 
@@ -13,6 +18,12 @@ const i18n = createI18n({
   locale: 'be',
   messages: messages
 })
+
+app.component('logo-icon', LogoIcon)
+app.component('rules-icon', RulesIcon)
+app.component('practice-icon', PracticeIcon)
+app.component('prize-icon', PrizeIcon)
+app.component('competition-icon', CompetitionIcon)
 
 app.use(router).use(store).use(vuetify).use(i18n)
 
