c9193fa79b42f364aa699f6ac79640ddaad908da
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-24 18:51:11 +0300

    быў імпартаваны кастамны абразок тэлеграма

---
+++ src/components/RatioSocialNetworksBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn icon v-for="(elem, key) in props.networks" :key="key" :href="elem">
+    <v-icon color="#DD0426" v-if="key == 'email'">mdi-{{ key }}-outline</v-icon>
+    <TelegramIcon color="#DD0426" v-else-if="key == 'telegram'" />
+    <v-icon color="#DD0426" v-else>mdi-{{ key }}</v-icon>
+  </v-btn>
+</template>
+
+<script setup>
+import TelegramIcon from './icons/TelegramIcon.vue'
+const props = defineProps({
+  networks: Object
+})
+</script>
+
+<style></style>
