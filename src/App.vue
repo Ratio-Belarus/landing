234bedccc8cd93e96bd4db02cfdf6ed935f93e53
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-24 18:57:28 +0300

    была выпраўлена памылка ў назве сацыяльнай сеткi

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
-<script setup>
-import logo_icon from './assets/logo-icon.png'
-</script>
-
 <template>
   <v-app>
-    <v-app-bar color="white" elevation="0" scroll-behavior="hide">
-      <v-app-bar-title>
-        <v-img :src="logo_icon" width="155"></v-img>
-      </v-app-bar-title>
-      <v-spacer />
-      <v-btn icon>
-        <v-icon color="#DD0426">mdi-twitter</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon color="#DD0426">mdi-instagram</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon color="#DD0426">mdi-email-outline</v-icon>
-      </v-btn>
-      <v-btn color="#DD0426" variant="flat" class="text-white text-capitalize font-weight-medium">{{
-        $t('btn.join')
-      }}</v-btn>
-    </v-app-bar>
+    <RatioHeader :networks="networks" />
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
+
+<script setup>
+import RatioHeader from './components/RatioHeader.vue'
+
+const networks = {
+  twitter: 'https://twitter.com',
+  instagram: 'https://www.instagram.com',
+  email: '#'
+}
+</script>
 
 <style scoped></style>
