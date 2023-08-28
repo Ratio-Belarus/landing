1cdb07e71006bad22a9cc47f029fad4e97bdc4ba
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-28 19:40:23 +0300

    дададзены кампанент выбару мовы старонкi

--- src/components/RatioHeader.vue
+++ src/components/RatioHeader.vue
@@ -17,16 +17,46 @@
       class="text-white text-capitalize font-weight-medium ml-6"
       >{{ $t('btn.join') }}</v-btn
     >
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn
+          variant="outlined"
+          size="large"
+          class="text-product-red text-uppercase font-weight-medium ml-6"
+          v-bind="props"
+          >{{ usedLang }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(language, index) in languages" :key="index">
+          <v-list-item-title class="text-uppercase" @click="chooseLang">{{
+            language
+          }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
 import RatioSocialNetworksBar from '../components/RatioSocialNetworksBar.vue'
 
 const props = defineProps({
   networks: Object,
   ratioFormLink: String
 })
+
+const languages = ['be', 'ru', 'en']
+
+const store = useStore()
+
+const usedLang = ref(store.state.language)
+
+function chooseLang(e) {
+  console.log(e.target.innerText.toLowerCase())
+}
 </script>
 
 <style scoped></style>
