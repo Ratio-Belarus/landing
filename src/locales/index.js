953ca78c2df83952cf88e6beafacc4c9bc077b0a
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-28 19:56:11 +0300

    дададзена праверка наяўнасцi мовы для гэтага сайта ў лакальным сховiшчы карыстальнiка

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,7 +2,7 @@ import { createI18n } from 'vue-i18n'
 import messages from './messages'
 
 const i18n = createI18n({
-  locale: 'be',
+  locale: localStorage.lang ? localStorage.lang : 'be',
   fallbackLocale: 'be',
   messages: messages
 })
