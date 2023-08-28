1dabc075258ea031de3618ceeb1c629ff54df603
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-28 15:34:13 +0300

    у сховiшча дададзена мова

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,8 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import messages from './messages'
+import store from '../store'
+
+const language = store.state.language
 
 const i18n = createI18n({
-  locale: 'be',
+  locale: language,
+  fallbackLocale: 'be',
   messages: messages
 })
 
