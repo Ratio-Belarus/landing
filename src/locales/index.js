6392c49ce755916924af4799179ffe32fe295ae5
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-28 16:02:59 +0300

    часова ўстаноўлена значэнне лакалi як беларуская мова

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,11 +1,8 @@
 import { createI18n } from 'vue-i18n'
 import messages from './messages'
-import store from '../store'
-
-const language = store.state.language
 
 const i18n = createI18n({
-  locale: language,
+  locale: 'be',
   fallbackLocale: 'be',
   messages: messages
 })
