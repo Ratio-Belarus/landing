eebe5e9afb23149bff00a884b45c3cf3cc252762
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-29 20:30:12 +0300

    кнопка была вынесена ў асобны кампанент

---
+++ src/components/RatioBtn.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn
+    color="#DD0426"
+    variant="flat"
+    :size="
+      widthClient <= 450 ? 'small' : widthClient > 450 && widthClient <= 960 ? 'large' : 'x-large'
+    "
+    :href="ratioFormLink"
+    target="blank"
+    class="text-white text-capitalize font-weight-medium btn-large"
+    >{{ props.text }}</v-btn
+  >
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const props = defineProps({
+  text: String
+})
+
+const store = useStore()
+
+const ratioFormLink = store.state.ratioFormLink
+
+const widthClient = computed(() => store.state.widthClient)
+</script>
+
+<style></style>
