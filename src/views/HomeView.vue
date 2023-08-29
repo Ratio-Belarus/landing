1bbf82c93c2ede97cbbf31f4d83e0c35f8b42373
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-08-29 13:31:42 +0300

    перароблена структура праекта: былi дададзены кампаненты кантэйнера, радкi i слупкi ў некаторых раздзелах

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,66 +72,74 @@ function submit() {
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
-        <h3 class="text-h4 font-weight-bold">{{ $t('title.func') }}</h3>
-      </v-col>
+      <v-container class="pa-0">
+        <v-row>
+          <v-col cols="12">
+            <h3 class="text-h4 font-weight-bold">{{ $t('title.func') }}</h3>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col v-for="card in functionCards" :cols="6" class="pa-0">
+            <div class="d-flex align-center">
+              <component :is="card.type + '-icon'" :width="54" class="flex-0-0" />
+              <v-card elevation="0" :text="card.text">
+                <template v-slot:title>
+                  <div class="d-flex align-center font-weight-bold">
+                    <p class="text-28">{{ card.title }}</p>
+                  </div>
+                </template>
+              </v-card>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-row>
     <v-row>
-      <v-col v-for="card in functionCards" :cols="6" class="pa-0">
-        <div class="d-flex align-center">
-          <component :is="card.type + '-icon'" :width="54" class="flex-0-0" />
-          <v-card elevation="0" :text="card.text">
-            <template v-slot:title>
-              <div class="d-flex align-center font-weight-bold">
-                <p class="text-28">{{ card.title }}</p>
-              </div>
-            </template>
-          </v-card>
-        </div>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <h3 class="text-h4 font-weight-bold">{{ $t('title.team') }}</h3>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col v-for="(card, i) in devCards" :key="i" :cols="6" class="col-dev">
-        <v-card elevation="0" class="fill-height">
-          <template v-slot:prepend>
-            <v-avatar size="112" :image="card.img"></v-avatar>
-          </template>
-          <template v-slot:title>
-            <p class="text-product-red text-28 font-italic font-weight-bold text-wrap">
-              {{ card.name }}
-            </p>
-          </template>
-          <template v-slot:subtitle>
-            <p class="text-product-red font-weight-medium text-wrap">{{ card.position }}</p>
-          </template>
-          <template v-slot:text>
-            <!-- Сommented out temporarily as there is no data yet-->
-            <RatioSocialNetworksBar
-              v-if="card.networks"
-              :networks="card.networks"
-              color="transparent"
-            />
-          </template>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" class="d-flex justify-center align-center">
-        <v-btn
-          color="#DD0426"
-          variant="flat"
-          size="large"
-          :href="ratioFormLink"
-          target="blank"
-          class="text-white text-capitalize font-weight-medium"
-          >{{ $t('btn.join') }}</v-btn
-        >
-      </v-col>
+      <v-container class="pa-0">
+        <v-row>
+          <v-col cols="12">
+            <h3 class="text-h4 font-weight-bold">{{ $t('title.team') }}</h3>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col v-for="(card, i) in devCards" :key="i" :cols="6" class="col-dev">
+            <v-card elevation="0" class="fill-height">
+              <template v-slot:prepend>
+                <v-avatar size="112" :image="card.img"></v-avatar>
+              </template>
+              <template v-slot:title>
+                <p class="text-product-red text-28 font-italic font-weight-bold text-wrap">
+                  {{ card.name }}
+                </p>
+              </template>
+              <template v-slot:subtitle>
+                <p class="text-product-red font-weight-medium text-wrap">{{ card.position }}</p>
+              </template>
+              <template v-slot:text>
+                <!-- Сommented out temporarily as there is no data yet-->
+                <RatioSocialNetworksBar
+                  v-if="card.networks"
+                  :networks="card.networks"
+                  color="transparent"
+                />
+              </template>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center align-center">
+            <v-btn
+              color="#DD0426"
+              variant="flat"
+              size="large"
+              :href="ratioFormLink"
+              target="blank"
+              class="text-white text-capitalize font-weight-medium"
+              >{{ $t('btn.join') }}</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-container>
     </v-row>
     <v-row>
       <v-col cols="12" class="mb-15">
