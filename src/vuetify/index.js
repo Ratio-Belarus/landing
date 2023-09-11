61a5c54e0d23d49b8cc079fb84d30f3b2d6a88e2
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-11 14:34:11 +0300

    падключана бiблiіятэка абразкоўfontawesome

--- src/vuetify/index.js
+++ src/vuetify/index.js
@@ -2,10 +2,19 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa4'
+import 'font-awesome/css/font-awesome.min.css'
 
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa
+    }
+  }
 })
 
 export default vuetify
