e61e1327158cccc90854751fbabcb01289d84abf
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-19 12:32:33 +0300

    дададзена мiнiмальная апрацоўка адказу з боку сервера пры адпраўцы формы

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,8 @@ const store = createStore({
       formFields: {
         contact: null,
         message: null
-      }
+      },
+      errorMessage: null
     }
   },
   mutations: {
@@ -25,6 +26,9 @@ const store = createStore({
     },
     SET_FORM_FIELDS(state, newFormFields) {
       state.formFields = newFormFields
+    },
+    SET_ERROR_MESSAGE(state, newErrorMessage) {
+      state.errorMessage = newErrorMessage
     }
   },
   actions: {
@@ -36,7 +40,19 @@ const store = createStore({
     },
     setFormFields({ commit }, newFormFields) {
       commit('SET_FORM_FIELDS', newFormFields)
-      axios.post(import.meta.env.VITE_APP_API_ENDPOINT_URL + '/contact_us', newFormFields)
+      axios
+        .post(import.meta.env.VITE_APP_API_ENDPOINT_URL + '/contact_us', newFormFields)
+        .then((res) => {
+          let errorMessage = {}
+          if (res.status == 200) {
+            errorMessage.type = 'success'
+          } else {
+            errorMessage.type = 'error'
+          }
+          errorMessage.text = res.data.data
+          commit('SET_ERROR_MESSAGE', errorMessage)
+          setTimeout(() => commit('SET_ERROR_MESSAGE', null), 2000)
+        })
     }
   }
 })
